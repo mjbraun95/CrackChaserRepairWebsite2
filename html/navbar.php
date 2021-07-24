722fef2b7cfcebc36396b5e4ea3a9e20fc81401a
Matthew Braun <mjbraun@ualberta.ca>
2021-07-23 19:04:57 -0600

    Edited navbar, formatted text boxes

--- html/navbar.php
+++ html/navbar.php
@@ -4,7 +4,7 @@
         <a href="about.php" <?php if ($currentPage === 'About') {echo 'class="active"';} ?>>About</a></li>
         <a href="services.php" <?php if ($currentPage === 'Services') {echo 'class="active"';} ?>>Services</a></li>
         <a href="contact.php" <?php if ($currentPage === 'Contsact') {echo 'class="active"';} ?>>Contact</a></li>
-        <a href="getaquote.php" <?php if ($currentPage === 'Get A Quote') {echo 'class="active"';} ?>>Get A Quote</a></li>
+        <!-- <a href="getaquote.php" <?php if ($currentPage === 'Get A Quote') {echo 'class="active"';} ?>>Get A Quote</a></li> -->
         <a href="beforeandafter.php" <?php if ($currentPage === 'Before & After') {echo 'class="active"';} ?>>Before & After</a></li>
         
     </div>
